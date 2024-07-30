eee737cc5ba19f9ca7c9975a6ee76eb29b69533e
Alexander <alexanderkkampfer@gmail.com>
2024-07-30 12:31:19 +1000

    attempt5

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Portfolio-site/", 
-  plugins: [react()]
+  plugins: [react()],
+  base: "/Portfolio-site/",
 })
